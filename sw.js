b501e36e1962238ca207ffeac4a0d6a790874337
qbikmuzik <pj.ehsani@gmail.com>
2026-06-11 22:33:27 -0500

    Deploy SMQ PWA to gh-pages branch

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const V = 'smq-v3';
-const ASSETS = ['/', '/index.html', '/css/app.css', '/js/app.js', '/js/fluid-simulation.js', '/manifest.webmanifest'];
+const ASSETS = ['/smq-pwa/', '/smq-pwa/index.html', '/smq-pwa/css/app.css', '/smq-pwa/js/app.js', '/smq-pwa/js/fluid-simulation.js', '/smq-pwa/manifest.webmanifest'];
 self.addEventListener('install', e => e.waitUntil(caches.open(V).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
